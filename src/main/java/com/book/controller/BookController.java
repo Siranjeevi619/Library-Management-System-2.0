09dd128905ec1da7615928d6abc277af61463d02
Siranjeevi619 <siranjeevi0619@gmail.com>
2025-06-30 20:07:17 +0530

    feat:added getBookById controller

--- src/main/java/com/book/controller/BookController.java
+++ src/main/java/com/book/controller/BookController.java
@@ -3,10 +3,9 @@ package com.book.controller;
 import com.book.Model.Book;
 import com.book.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/book")
@@ -14,6 +13,20 @@ public class BookController {
 
     @Autowired
     private BookService bookService;
+
+
+    @GetMapping("/all")
+    public List<Book> bookList(){
+        return bookService.findAll();
+    }
+
+
+    @GetMapping("/{id}")
+    public Book bookListById(@PathVariable int id){
+        return bookService.findById((long)id);
+    }
+
+
 
     @PostMapping("/add")
     public Book addBook(@RequestBody Book book) {
